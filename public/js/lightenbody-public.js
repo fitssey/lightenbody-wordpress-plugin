b43382a53f216e0c61d55b136ad3fda0ca66f7ce
Grzegorz Tomasiak <grzegorztomasiak@live.com>
2016-08-10 21:37:44 +0200

    Fixed target of the link in magnificPopup.

--- public/js/lightenbody-public.js
+++ public/js/lightenbody-public.js
@@ -29,7 +29,7 @@
 	 * practising this, we should strive to set a better example in our own work.
 	 */
 
-	$('.lightenbody-schedule-signup-link').magnificPopup({
+	$('.lb-schedule-body-booking-link').magnificPopup({
 		type: 'iframe'
 	});
 
